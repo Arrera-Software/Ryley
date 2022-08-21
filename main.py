bdce7844b42606b43037aa94aa241cda074af017
Wiruto2 <vandewallewil@gmail.com>
2022-08-21 21:57:17 +0200

    Boutton

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ from translate import translate
 import time
 
 screen = Tk()
+def Meteo():
+    print("Okkkkk")
 screen.title("Ryley")
 screen.maxsize(500,700)
 screen.minsize(500,700)
 screen.iconphoto(False,PhotoImage(file="image/Ryley.png"))
-
+B1 = Button (text="meteo",command=Meteo).pack
 screen.mainloop()
