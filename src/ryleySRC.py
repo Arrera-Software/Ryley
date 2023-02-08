43720f0fa19c43cdd210a1a9dc8a2fc1f523f767
Baptiste Pauchet <63608854+SpeedyCreator@users.noreply.github.com>
2023-02-08 12:31:01 +0100

    ajout d'un passage a ligne au bout de 7 mots

--- src/ryleySRC.py
+++ src/ryleySRC.py
@@ -4,7 +4,22 @@ import time
 
 
 class RyleySRC :
+    def passageLigne(text, nbMots):
+        mots = text.split()
+        newText = ""
+        ligne = ""
+        for i, mot in enumerate(mots):
+            ligne += mot + " "
+            if (i + 1) % nbMots == 0:
+                newText += ligne + "\n"
+                ligne = ""
+            elif i == len(mots) - 1:
+                newText += ligne
+        return newText
+    
     def speak(text,label,nom):
+        label.config(justify="left")
+        text = RyleySRC.passageLigne(text,7)
         label.config(text=nom+": "+text)
         label.update()
     
@@ -21,3 +36,4 @@ class RyleySRC :
                 return "None" 
             time.sleep(1)
             return Requette
+    
