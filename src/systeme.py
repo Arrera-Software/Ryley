8caa0a02cff1af28fae8542e8f83d41476c9b8c4
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2023-06-29 22:32:21 +0200

    vixe probleme

--- src/systeme.py
+++ src/systeme.py
@@ -6,7 +6,7 @@ secondColor = "#011ddd"
 secondTextColor = "white"
 
 software1= ObjetSoftware("soft1")
-software2= ObjetSoftware("soft1")
-software3= ObjetSoftware("soft1")
-software4= ObjetSoftware("soft1")
-software5= ObjetSoftware("soft1")
+software2= ObjetSoftware("soft2")
+software3= ObjetSoftware("soft3")
+software4= ObjetSoftware("soft4")
+software5= ObjetSoftware("soft5")
